51a1b7e99dc35cb8f2d03c6ef73eb0ef5ab97476
thanhphat18 <92084016+thanhphat18@users.noreply.github.com>
2023-12-17 22:05:42 +0700

    Update Mine.java

--- src/Entity/Mine.java
+++ src/Entity/Mine.java
@@ -32,12 +32,66 @@ public class Mine {
     }
 
     public void firstClick(int x, int y){
+        Random rand = new Random();
 
+        for(int i = 0; i < column; i++){
+            for(int j = 0; j < row; j++){
+                if (i <= x + 1 && i >= x - 1 && j <= y + 1 && j >= y - 1){
+                    mines[i][j] = 0;
+                    revealed[i][j] = true;
+                    flagged[i][j] = false;
+                    continue;                    
+                }
+
+                if (rand.nextInt(100) < 20){
+                    mines[i][j] = 1;
+                } else {
+                    mines[i][j] = 0;
+                }
+                revealed[i][j] = false;
+                flagged[i][j] = false;
+            }
+        }
+        
+        for(int i = 0; i < column; i++){
+            for(int j = 0; j < row; j++){
+                int neighs = 0;
+                for(int m = 0; m < column; m++){
+                    for(int n = 0; n < row; n++){
+                        if (isN(i, j, m, n) == true){
+                            neighs++;
+                        }
+                    }
+                }
+                neighbors[i][j] = neighs;
+            }
+        }
                 
     }
 
     public void reset(){
-
+        for(int i = 0; i < column; i++){
+            for(int j = 0; j < row; j++){
+                mines[i][j] = 0;
+                revealed[i][j] = false;
+                flagged[i][j] = false;
+            }
+        }
+        for(int i = 0; i < column; i++){
+            for(int j = 0; j < row; j++){
+                int neighs = 0;
+                for(int m = 0; m < column; m++){
+                    for(int n = 0; n < row; n++){
+                        if (isN(i, j, m, n) == true){
+                            neighs++;
+                        }
+                    }
+                }
+                neighbors[i][j] = neighs;
+            }
+        }
+        count = 0;
+        board.clearStack();
     }    
 
     public void mouseMoved(MouseEvent e){
@@ -57,7 +111,22 @@ public class Mine {
     }
 
     public void emptyCell(int i, int j){
+        for(int a = i - 1; a <= i + 1;a++){
+            for(int b = j - 1; b <= j + 1;b++){
+                if (a == i && b == j){
+                    continue;
+                }
 
+                if (a < column && a >= 0 && b < row && b >= 0){
+                    if (getMines()[a][b] == 0){
+                        if (!getRevealed()[a][b]){
+                            getRevealed()[a][b] = true;
+                            board.pushStask(a,b, Math.max(this.column, this.row)*(Math.max(this.column, this.row) + 1));
+                        }
+                    }                    
+                }
+            }
+        }
     }
 
     public int totalMines(){
